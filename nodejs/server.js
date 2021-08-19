321c720033e08a42827d7f649a5029b486144b47
DESKTOP-BND2T4B\max <a0928345634@gmail.com>
2021-08-19 17:16:28 +0800

    node.js get 請求入門篇 第三課

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,12 +2,12 @@ const http = require("http");
 const fs = require ("fs");//為了要讀取服務器裡的文件，導入fs模塊=file system 可使用fs.readfile讀取html文件
 const sendResponse = (filename,statusCode,response)=>
 {
-fs.readFile(`./html/${filename}`,(error,data)=>
+fs.readFile(`./html/${filename}`,(error,data)=>//注意要點在./之後要輸入正確資料夾的位置,像原本存放位置之前有多一個資料夾結果就會報錯
 {
-    if (error)
+    if (error)//仙藥判斷是否error
     {
     response.statusCode= 500;
-    response.setHeader("Content-Type","text/plain")
+    response.setHeader("Content-Type","text/plain");
     response.end("sorry internal erroe");
     }
     else{
@@ -21,30 +21,36 @@ fs.readFile(`./html/${filename}`,(error,data)=>
 };
 
 const server = http.createServer((request,response)=>
-{  console.log(request.url,request.method);
-
+{  console.log(request.url,request.method);//(request.url 告知node 後端程序用戶想要法問哪個頁面,method請求方法)
+    
     const method = request.method;
     const url = request.url;
 
-if(method ==="GET")
+if(method ==="GET")//判斷METHOD
 {
-    if(url==="/"){}
-    else if(url==="/about.html") {  }
+    if(url==="/")//"/"=>判斷跟目錄也就是主頁的意思
+    {
+      sendResponse("index.html",200,response);
+    }
 
+    else if(url==="/about.html") 
+    { 
+        sendResponse("about.html",200,response);
+     }
+
+else
+{
+sendResponse("404.html",404,response)
+}
 }
 else
 {
 
-
-
-
 }
 
-
-
-    response.end("hellow from nodejs server !");
-}
-);
+   
+})
+;
 
 
 const port = 3000;
